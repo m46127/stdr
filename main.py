28773b5f89f49d9a64b413c691d74337dba4923c
m46127 <hirotsuguono7@gmail.com>
2023-11-14 14:07:02 +0900

    first_commit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # メニューの選択肢を定義
-menu_options = ["トップページ", "ピッキング", "CSV並び替え", "PDF作成", "在庫"]
+menu_options = ["トップページ", "ピッキング", "CSV並び替え", "PDF作成", "在庫", "同梱物集計"]
 
 # サイドバーでオプションメニューを表示
-selected_option = option_menu("メインメニュー", menu_options, icons=['house', 'upload', 'sort', 'file-pdf'], menu_icon="cast", default_index=0)
+selected_option = option_menu("メインメニュー", menu_options, icons=['house', 'upload', 'sort', 'file-pdf', 'layers', 'box'], menu_icon="cast", default_index=0)
 
 # 選択肢に応じて表示するページを変更
 if selected_option == "トップページ":
@@ -33,3 +33,8 @@ elif selected_option == "在庫":
     # Inventory.py の内容をインポートして実行
     from Inventory import main as Inventory_main
     Inventory_main()
+
+elif selected_option == "同梱物集計":
+    # Include.py の内容をインポートして実行
+    from Include import main as Include_main
+    Include_main()
