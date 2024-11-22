f7d72dcb99442f42d5b43d990c8b955f6d79a2b8
m46127 <hirotsuguono7@gmail.com>
2024-11-22 15:04:58 +0900

    first_commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/hirotsugu/Desktop/ship_dr/venv"
+set -gx VIRTUAL_ENV "/Users/hirotsugu/Desktop/stdr/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
